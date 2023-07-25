8a68e606f95ccbca1e2b3862e16a2507d7e9ce9e
Yahagi Kota <s2111511@coins.tsukuba.ac.jp>
2023-07-25 17:57:11 +0900

    必ず選択

--- static/javascript/exhibit.js
+++ static/javascript/exhibit.js
@@ -1,3 +1,32 @@
 function showAlert() {
   alert('出品しました！');
 }
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  function validateCheckBoxes() {
+    let checkedCount = 0;
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        checkedCount++;
+      }
+    });
+
+    if (checkedCount === 0) {
+      alert('最低でも一つの項目を選択してください。');
+      return false;
+    }
+    showAlert()
+    return true;
+
+  }
+
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    if (!validateCheckBoxes()) {
+      event.preventDefault();
+      
+    }
+  });
+
